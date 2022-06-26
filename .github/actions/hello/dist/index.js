560796fba250f6a4ca06605f462d91174f2d0a0a
dhandstg <dutra.h.andre.cursos@gmail.com>
2022-06-26 12:23:03 +0200

    fix: compiled index.js and its dependencies with the correction made in previous commit

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -887,8 +887,8 @@ try
     const name = core.getInput('whom-to-greet');
     console.log(`Hello, ${name}`);
     const time = new Date();
-    core.setOutput("time", time.toTimeString);
-    //Let's checl what is inside the github object!
+    core.setOutput("time", time.toTimeString());
+    //Let's check what is inside the github object!
     console.log(JSON.stringify(github, null, '\t'))
 
     //In order to force-fail an action, we

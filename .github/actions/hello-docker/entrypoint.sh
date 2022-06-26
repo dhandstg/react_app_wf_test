9e8b1bf89e5af3428dc8f58f9081b05a99ecb351
dhandstg <dutra.h.andre.cursos@gmail.com>
2022-06-26 15:36:13 +0200

    ci: added a new action

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+
+echo "::debug ::This is a debug message"
+echo "::warning ::This is a warning message"
+echo "::error ::This is an error message"
+
+echo "::add-mask::$1"
+
+time = $(date)
+echo "::set-output name=time::$time"
+
+echo "::group::Expandable logs with 8 lines"
+for num in 1 2 3 4 5 6 7 8
+do
+    echo "Line $num from group"
+done
+echo "::endgroup::"
+
+echo "::set-env name=HELLO::hello"

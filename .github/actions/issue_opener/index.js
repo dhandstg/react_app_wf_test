17c9d550f5978a4008125f2b505bf459e4adefb5
dhandstg <dutra.h.andre.cursos@gmail.com>
2022-06-26 13:48:54 +0200

    fix: post request -4

--- .github/actions/issue_opener/index.js
+++ .github/actions/issue_opener/index.js
@@ -23,7 +23,7 @@ try
     
     const octokit = new github.getOctokit(githubToken);
     response = null;
-    send_post(octokit, github, title, body, assignees).then(resp => core.setOutput('issue', JSON.stringify(resp.data))).catch(e => console.log(e));
+    send_post(octokit, github, title, body, assignees).then(resp => core.setOutput('issue', JSON.stringify(resp))).catch(e => console.log(e));
     
 }
 catch (error) {
